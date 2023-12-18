fb7c34c1b9aa614075ec64747c01e11811e7714c
godsakani <nyonggodwill11@gmail.com>
2023-12-18 05:34:28 +0100

    Added app globals

---
+++ lib/global/styles/assets_string.dart
@@ -0,0 +1,26 @@
+const IMAGE_ASSETS = "assets/images";
+const ICONS = "assets/icons";
+
+class ImageAssets {
+   static String logo = '$IMAGE_ASSETS/logo.png';
+   static String notification = '$ICONS/notification.svg';
+  static String selectHome = "$ICONS/selected_home.svg";
+  static String unSelectHome = "$ICONS/unselected_home.svg";
+  static String unSelectHospital = '$ICONS/unselected_hospital.svg';
+   static String selectHospital = '$ICONS/selected_hospital.svg';
+   static String unSelectedBlog = "$ICONS/unselected_blog.svg";
+   static String selectedBlog = "$ICONS/selected_blog.svg";
+   static String unSelectedUser = '$ICONS/unselected_user.svg';
+   static String selectedUser = '$ICONS/selected_user.svg';
+   static String hospital = '$ICONS/hospital.svg';
+   static String blog = '$ICONS/blog.svg';
+   static String location = "$ICONS/location.svg";
+   static String doctor = "$IMAGE_ASSETS/doctor.png";
+   static String myHospital = "$IMAGE_ASSETS/placeholder_image.png";
+   static String topBlog = "$IMAGE_ASSETS/apollo.jpg";
+   static String profile = "$IMAGE_ASSETS/lady.jpeg";
+   static String close = "$ICONS/x.svg";
+   static String user = "$IMAGE_ASSETS/user.png";
+   static String  home = "$ICONS/home.svg";
+   static String user1 = "$ICONS/unselected_user.svg";
+}
---
+++ lib/global/theme/themeScreen.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:mboacare/global/styles/appStyles.dart';
+import 'package:mboacare/global/styles/colors.dart';
+import 'package:mboacare/global/theme/themeConstants.dart';
+import 'package:provider/provider.dart';
+
+class ThemeScreen extends StatefulWidget {
+  const ThemeScreen({super.key});
+
+  @override
+  State<ThemeScreen> createState() => _ThemeScreenState();
+}
+
+class _ThemeScreenState extends State<ThemeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<ThemeProvider>(builder: (context, themeProvider, child) {
+      //final themeProvider = Provider.of<ThemeProvider>(context, listen: false);
+      final themeMode = themeProvider.themeMode;
+      return Scaffold(
+        body: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.only(right: 10),
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            Row(
+              children: [
+                IconButton(
+                  onPressed: Navigator.of(context).pop,
+                  icon: const Icon(
+                    Icons.arrow_back_rounded,
+                    size: 25,
+                  ),
+                ),
+                const Text(
+                  'Back',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(left: 4.0),
+                    child: Text(
+                      'App theme',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: AppColors.textColor2,
+                          fontSize: 16),
+                    ),
+                  ),
+                  const Text(
+                    ' Set application theme',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        color: AppColors.textColor2,
+                        fontSize: 16),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Padding(
+                          padding: EdgeInsets.only(left: 4.0),
+                          child: Text(
+                            'System settings',
+                            style: TextStyle(
+                                fontWeight: FontWeight.w400,
+                                // color: AppColors.textColor2,
+                                fontSize: 16),
+                          )),
+                      Radio(
+                          fillColor: MaterialStateColor.resolveWith(
+                              (states) => AppColors.textColor2),
+                          value: ThemeMode.system,
+                          groupValue: themeMode,
+                          onChanged: (selectedTheme) async {
+                            await themeProvider.toggleTheme(selectedTheme);
+                          }),
+                    ],
+                  ),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        const Padding(
+                            padding: EdgeInsets.only(left: 4.0),
+                            child: Text(
+                              'Light mode',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w400,
+                                  // color: AppColors.textColor2,
+                                  fontSize: 16),
+                            )),
+                        Radio(
+                            fillColor: MaterialStateColor.resolveWith(
+                                (states) => AppColors.textColor2),
+                            focusColor: MaterialStateColor.resolveWith(
+                                (states) => AppColors.textColor2),
+                            // toggleable: ,
+                            value: ThemeMode.light,
+                            groupValue: themeMode,
+                            onChanged: (selectedTheme) async {
+                              await themeProvider.toggleTheme(selectedTheme);
+                              //clickedOption = value;
+                            }),
+                      ]),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                          padding: EdgeInsets.only(left: 4.0),
+                          child: Text(
+                            'Dark mode',
+                            style: AppTextStyles.bodyOne.copyWith(
+                                fontWeight: FontWeight.w400,
+                                // color: AppColors.textColor2,
+                                fontSize: 16),
+                          )),
+                      Radio(
+                          fillColor: MaterialStateColor.resolveWith(
+                              (states) => AppColors.textColor2),
+                          value: ThemeMode.dark,
+                          groupValue: themeMode,
+                          onChanged: (selectedTheme) async {
+                            await themeProvider.toggleTheme(selectedTheme);
+                            //clickedOption = value;
+                          }),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ]),
+        )),
+      );
+    });
+  }
+}
