c10b3e722391628767974de4f7bed7db95d3ae75
Sparklinglily <carlaejemeh@gmail.com>
2023-12-20 10:31:10 +0100

    holla

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mboacare/app_modules/user/screens/inner_screen/aboutUs.dart';
+import 'package:mboacare/app_modules/user/screens/inner_screen/user_profile_page.dart';
 import 'package:mboacare/global/theme/themeConstants.dart';
+import 'package:mboacare/global/theme/themeScreen.dart';
 import 'package:mboacare/services/signup_provider.dart';
 import 'package:mboacare/app_modules/splashscreen/splash.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -41,58 +44,63 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        title: 'Mboacare',
-        debugShowCheckedModeBanner: false,
-       // themeMode: Provider.of<ThemeProvider>(context).themeMode,
-        theme: Provider.of<ThemeProvider>(context).themeMode == ThemeMode.dark
-            ? ThemeData.dark().copyWith(
-                // colorScheme: ,
-                scaffoldBackgroundColor: DarkThemeColors.background,
-                cardColor: DarkThemeColors.cardBackground,
-                primaryColor: DarkThemeColors.primaryText,
-                //accentColor: DarkThemeColors.accentColor,
-                //buttonColor: DarkThemeColors.buttonBackground,
-                textTheme: const TextTheme(
-                  headlineSmall: TextStyle(
-                    color: DarkThemeColors.primaryText,
-                  ),
-                  bodyMedium: TextStyle(
-                    color: DarkThemeColors.secondaryText,
-                  ),
+      title: 'Mboacare',
+      debugShowCheckedModeBanner: false,
+      // themeMode: Provider.of<ThemeProvider>(context).themeMode,
+      theme: Provider.of<ThemeProvider>(context).themeMode == ThemeMode.dark
+          ? ThemeData.dark().copyWith(
+              // colorScheme: ,
+              scaffoldBackgroundColor: DarkThemeColors.background,
+              cardColor: DarkThemeColors.cardBackground,
+              primaryColor: DarkThemeColors.primaryText,
+              //accentColor: DarkThemeColors.accentColor,
+              //buttonColor: DarkThemeColors.buttonBackground,
+              textTheme: const TextTheme(
+                headlineSmall: TextStyle(
+                  color: DarkThemeColors.primaryText,
                 ),
-              )
-            : ThemeData.light(),
-        // theme: ThemeData.light(),
-        // darkTheme: ThemeData.dark(),
-        //notifier.darkTheme ? themeLight : themeDark,
-        //     ThemeData(
-        //   colorScheme:
-        //       ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
-        //   useMaterial3: true,
-        // ),
-        // Add supported locales and localizations delegates
-        supportedLocales: const [
-          Locale('en', 'US'), // English
-          Locale('hi', 'IN'), // Hindi
-          Locale('es', 'ES'), // Spanish
-          Locale('fr', 'FR'), // French
-          // Add more locales here for other languages
-        ],
-        localizationsDelegates: const [
-          AppLocalizations.delegate,
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-        ],
-        localeResolutionCallback: (locale, supportedLocales) {
-          for (var supportedLocale in supportedLocales) {
-            if (supportedLocale.languageCode == locale?.languageCode &&
-                supportedLocale.countryCode == locale?.countryCode) {
-              return supportedLocale;
-            }
+                bodyMedium: TextStyle(
+                  color: DarkThemeColors.secondaryText,
+                ),
+              ),
+            )
+          : ThemeData.light(),
+      // theme: ThemeData.light(),
+      // darkTheme: ThemeData.dark(),
+      //notifier.darkTheme ? themeLight : themeDark,
+      //     ThemeData(
+      //   colorScheme:
+      //       ColorScheme.fromSeed(seedColor: AppColors.primaryColor),
+      //   useMaterial3: true,
+      // ),
+      // Add supported locales and localizations delegates
+      supportedLocales: const [
+        Locale('en', 'US'), // English
+        Locale('hi', 'IN'), // Hindi
+        Locale('es', 'ES'), // Spanish
+        Locale('fr', 'FR'), // French
+        // Add more locales here for other languages
+      ],
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      localeResolutionCallback: (locale, supportedLocales) {
+        for (var supportedLocale in supportedLocales) {
+          if (supportedLocale.languageCode == locale?.languageCode &&
+              supportedLocale.countryCode == locale?.countryCode) {
+            return supportedLocale;
           }
-          return supportedLocales.first;
-        },
-        home: const SplashScreen(),
-        );
+        }
+        return supportedLocales.first;
+      },
+      home: const SplashScreen(),
+      routes: {
+        ('/aboutUs'): (context) => AboutUs(),
+        ('/themeScreen'): (context) => ThemeScreen(),
+        ('/profilePage'): (context) => ProfilePage(),
+      },
+    );
   }
 }
