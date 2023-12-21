4bcf069eaf754b2705f1b4eef8487839fb2274d8
godsakani <nyonggodwill11@gmail.com>
2023-12-21 09:27:05 +0100

    Modified user dashboard

--- lib/app_modules/user/user_dashboard.dart
+++ lib/app_modules/user/user_dashboard.dart
@@ -6,6 +6,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:mboacare/app_modules/user/screens/dashboard/blog_page.dart';
 import 'package:mboacare/global/styles/colors.dart';
+import 'package:mboacare/services/map_services/permision.dart';
 import '../../global/styles/assets_string.dart';
 import '../../widgets/drawer_widget.dart';
 import '../../widgets/myBottomBar.dart';
@@ -24,6 +25,14 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class _DashboardScreenState extends State<DashboardScreen> {
+
+  
+  @override
+  void didChangeDependencies() {
+    locatePosition(context);
+    super.didChangeDependencies();
+  }
+
   int _currentIndex = 0;
  
   final List<Widget> _pages = [
