715cd0b9fbeccd4b135687fe41e37cad863b01c0
godsakani <nyonggodwill11@gmail.com>
2023-12-22 07:30:54 +0100

    Added account success screen and remove unuseed screen

---
+++ lib/app_modules/auth/success_screen.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mboacare/global/styles/appStyles.dart';
+import 'package:mboacare/global/styles/assets_string.dart';
+import 'package:mboacare/global/styles/colors.dart';
+
+import 'login.dart';
+
+class SuccessScreen extends StatelessWidget {
+  const SuccessScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: height * 0.25,
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Image.asset(
+              ImageAssets.checkedRing,
+              height: 150,
+              width: 150,
+            ),
+          ),
+          Text(
+            'Account Created successfully!.',
+            textAlign: TextAlign.center,
+            style: AppTextStyles.bodyOne
+                .copyWith(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            'Kindly Check your email for account verification!!',
+            textAlign: TextAlign.center,
+            style: AppTextStyles.bodyFour
+                .copyWith(fontSize: 16, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          ElevatedButton(
+            onPressed: () {
+              //call the routing here
+              Get.to(() => const LoginScreen(title: ''));
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: AppColors.buttonColor,
+              foregroundColor: AppColors.whiteColor,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5),
+              ),
+              minimumSize: const Size(150, 45),
+            ),
+            child: const Text(
+              'Login',
+              style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
