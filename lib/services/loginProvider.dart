876b1aa34c1816fbd43cb625d0a00c41701807fc
godsakani <nyonggodwill11@gmail.com>
2024-01-03 02:20:58 +0100

    Added new endpoints, app api services file

--- lib/services/loginProvider.dart
+++ lib/services/loginProvider.dart
@@ -5,6 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:mboacare/app_modules/med_user/med_dashboard.dart';
+import 'package:mboacare/app_modules/user/user_dashboard.dart';
 import 'package:mboacare/utils/snack_error.dart';
 import 'package:mboacare/utils/snack_succ.dart';
 import 'package:mboacare/utils/validations.dart';
@@ -13,10 +14,15 @@ class LoginProvider extends ChangeNotifier {
   final auth = FirebaseAuth.instance;
   final googleSignIn = GoogleSignIn();
 
+ bool _loading = false;
   String _reqMessage = "";
   bool _isLoading = false;
-   final emailController = TextEditingController();
+  
+  final emailController = TextEditingController();
   final passwordController = TextEditingController();
+  String userEmail = '';
+  String userName = '';
+  String profileImage = "";
   String email = "";
   String password = "";
   bool isPasswordVisible = false;
@@ -24,7 +30,8 @@ class LoginProvider extends ChangeNotifier {
   bool isValidSignIn = false;
   String get reqMessage => _reqMessage;
   bool get isLoading => _isLoading;
- setEmail(String value) {
+  bool get loading => _loading;
+  setEmail(String value) {
     email = value;
     notifyListeners();
   }
@@ -42,7 +49,10 @@ class LoginProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  void login({required String email, required String password, required BuildContext context}) async {
+  void login(
+      {required String email,
+      required String password,
+      required BuildContext context}) async {
     _isLoading = true;
     notifyListeners();
 
@@ -56,16 +66,18 @@ class LoginProvider extends ChangeNotifier {
       if (user!.emailVerified == false) {
         _isLoading = false;
         _reqMessage = "Email is not verified! Check email to verify Account!";
-        snackErrorMessage(message: "Email is not verified! Check email to verify Account!", context: context);
+        snackErrorMessage(
+            message: "Email is not verified! Check email to verify Account!",
+            context: context);
         print(_reqMessage);
         notifyListeners();
       } else {
         _isLoading = false;
-      
+
         snackMessage(message: "Login Successful!", context: context);
         print(user);
         notifyListeners();
-          Get.to(() => const MedDashboard(),
+        Get.to(() => const MedDashboard(),
             duration: const Duration(
               milliseconds: 800,
             ),
@@ -104,16 +116,19 @@ class LoginProvider extends ChangeNotifier {
           await auth.signInWithCredential(credential);
       final User user = userCredential.user!;
       print(user);
-    
+      userEmail = user.email!;
+      userName = user.displayName!;
+      profileImage = user.photoURL!;
       // Handle successful sign-in
-        if (user.emailVerified == false) {
+      if (user.emailVerified == false) {
         _isLoading = false;
-        _reqMessage = "Email is not verified! Check email to verify Account!";
+        snackErrorMessage(
+            message: "Email is not verified! Check email to verify Account!",
+            context: context);
         print(_reqMessage);
         notifyListeners();
       } else {
         _isLoading = false;
-      //  _reqMessage = "Login Successful!";
         snackMessage(message: "Login Successful!", context: context);
         print(user);
         notifyListeners();
@@ -124,24 +139,56 @@ class LoginProvider extends ChangeNotifier {
             curve: Curves.easeInCirc,
             transition: Transition.fadeIn);
       }
-     
     } catch (e) {
       notifyListeners();
       print(e.toString());
       if (e is FirebaseAuthException) {
         if (e.code == "user-not-found") {
           _isLoading = false;
+          snackErrorMessage(message: e.code, context: context);
           _reqMessage = e.code;
         } else {
           _isLoading = false;
-          _reqMessage = e.message!;
+          snackErrorMessage(message: e.message!, context: context);
           notifyListeners();
         }
       }
     }
   }
 
-   void clear() {
+  void signOut({required BuildContext context}) async {
+    _loading = true;
+    snackMessage(message: "Logout Successful!", context: context);
+    notifyListeners();
+    Get.to(() => const DashboardScreen(userName: ''),
+        duration: const Duration(
+          milliseconds: 800,
+        ),
+        curve: Curves.easeInCirc,
+        transition: Transition.fadeIn);
+    try {
+      await auth.signOut();
+      await googleSignIn.signOut(); // For Google sign-in
+      _loading = false;
+      notifyListeners();
+    } catch (error) {
+      // Handle signout errors
+      if (error is FirebaseAuthException) {
+        if (error.code == "user-not-found") {
+          _loading = false;
+          snackErrorMessage(message: error.code, context: context);
+          _reqMessage = error.code;
+        } else {
+          _loading = false;
+          snackErrorMessage(message: error.message!, context: context);
+          notifyListeners();
+        }
+      }
+      print(error);
+    }
+  }
+
+  void clear() {
     _reqMessage = '';
     notifyListeners();
   }
